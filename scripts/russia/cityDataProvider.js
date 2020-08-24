e2c7f57b94a75cee69d66163e13b88d69c10f562
Goody <mattgoodman3099@gmail.com>
2020-08-24 14:29:43 -0500

    .js crazy

--- scripts/russia/cityDataProvider.js
+++ scripts/russia/cityDataProvider.js
@@ -16,7 +16,7 @@ const cityCollection = [
         population: 1493749,
     },
     {
-        name: Kazan,
+        name: "Kazan",
         population: 1257391,
     },
     {
@@ -24,3 +24,42 @@ const cityCollection = [
         population: 1252236,
     },
 ]
+
+// makes a copy of cityCollection
+export const useCityData = () => {
+    return cityCollection.slice();
+}
+
+export const cityList = () => {
+
+
+    const contentElement = document.querySelector(".cityList")
+    const cities = useCityData()
+
+
+    let cityHTMLRepresentation = ""
+    for(const city of cities) {
+        cityHTMLRepresentation += City(city)
+    }
+
+
+// add to the existing HTML in the content element
+contentElement.innerHTML += `${cityHTMLRepresentation}`
+}
+
+
+export const City = (cityObject) => {
+    return `
+        <div class="cityListCard">
+            <div class="cardImage">
+
+                </div>
+                    <ul>
+                        <li>Name: ${cityObject.name}</li>
+                        <li>Population: ${cityObject.population}</li>
+                    </ul>
+        </div>`
+}
+
+
+cityList();
