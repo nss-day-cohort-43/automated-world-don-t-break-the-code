c9758ab8484360d312edace9c8423c93bf14068a
Goody <mattgoodman3099@gmail.com>
2020-08-24 15:47:30 -0500

    dont forget

--- scripts/russia/leaderDataProvider.js
+++ scripts/russia/leaderDataProvider.js
@@ -1,6 +1,40 @@
 const leaderCollection = [
     {
         name: "Vladimir Putin",
-        image: "images/vladimirPutin.jpg",
+        image: "vladimirPutin.jpg",
     },
 ]
+
+export const leaderCityData = () => {
+    return leaderCollection.slice();
+}
+
+export const leaderList = () => {
+
+
+    const contentElement = document.querySelector(".leaderList")
+    const leaders = leaderCityData()
+
+
+    let leaderHTMLRepresentation = ""
+    for(const leader of leaders) {
+        leaderHTMLRepresentation += Leader(leader)
+    }
+
+
+// add to the existing HTML in the content element
+contentElement.innerHTML += `${leaderHTMLRepresentation}`
+}
+
+
+export const Leader = (leaderObject) => {
+    return `
+        <div class="leaderListCard">
+            <div class="cardImage"> <div>${leaderObject.name}</div>
+            <img class="leader-image" src="images/${leaderObject.image}" alt="${leaderObject.name}">
+            </div>       
+        </div>`
+}
+
+
+leaderList();
