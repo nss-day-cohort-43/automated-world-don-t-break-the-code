c0b1b4a56d8bf3b4d2b7dfbe7ec19569694572ab
Goody <mattgoodman3099@gmail.com>
2020-08-24 13:06:15 -0500

    added images and array of objects

---
+++ scripts/russia/cityDataProvider.js
@@ -0,0 +1,26 @@
+const cityCollection = [
+    {
+        name: "Moscow",
+        population: 12480481,
+    },
+    {
+        name: "Saint Petersburg",
+        population: 4879566,
+    },
+    {
+        name: "Novosibirsk",
+        population: 1625631,
+    },
+    {
+        name: "Yekaterinburg",
+        population: 1493749,
+    },
+    {
+        name: Kazan,
+        population: 1257391,
+    },
+    {
+        name: "Nizhny Novgorod",
+        population: 1252236,
+    },
+]
---
+++ scripts/russia/landmarkDataProvider.js
@@ -0,0 +1,26 @@
+const landmarkCollection = [
+    {
+        name: "State Hermitage",
+        image: "images/stateHermitageMuseum.jpg",
+    },
+    {
+        name: "Kremlin",
+        image: "kermlin.jpg",
+    },
+    {
+        name: "Red Square",
+        image: "redSquare.jpg",
+    },
+    {
+        name: "Winter Palace",
+        image: "winterPalace.jpg",
+    },
+    {
+        name: "Peterhof",
+        image: "peterhof.jpg",
+    },
+    {
+        name: "Catherine Palace",
+        image: "catherinePalace.jpg",
+    },
+]
